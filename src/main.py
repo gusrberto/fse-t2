7a18e6b36c76c9027d19ecf5d0138788b086724a
gusrberto <gustavorsdemelo@gmail.com>
2025-01-19 15:59:22 -0300

    a

--- src/main.py
+++ src/main.py
@@ -3,13 +3,12 @@ from src.modules.pid import PID
 from src.modules.hall_sensors import HallSensors
 from src.modules.pedals import Pedals
 from src.modules.vehicle_control import VehicleControl
+import RPi.GPIO as GPIO
 import time
 import threading
 
 # Inicializando modulos
-#engine = Engine()
 engine_pid = PID()
-#pedals = Pedals()
 hall_sensors = HallSensors()
 vehicle_control = VehicleControl(max_speed=200)
 
@@ -23,11 +22,11 @@ routine_thread = None
 def main():
     global routine_thread
     try:
-        """ # Executa a calibração e verifica se foi bem-sucedida
-        if not calibrate_system():
+        # Executa a calibração e verifica se foi bem-sucedida
+        if not calibrate_system(vehicle_control, hall_sensors):
             print("Calibração falhou. Encerrando o sistema...")
             close()
-            return """
+            return
 
         # Inicia a rotina principal
         routine_thread = threading.Thread(target=routine)
@@ -69,50 +68,18 @@ def routine():
 
         time.sleep(sampling_period)
 
-""" def routine_com_timer():
-    global current_speed, timer
-
-    if not running:
-        return
-
-    # Get reference speed from pedals
-    #reference_speed = pedals.calculate_reference_speed(current_speed=current_speed)
-
-    # Update the PID controller with the new reference speed
-    #pid.refresh_reference(reference_speed)
-
-    # Read the current wheel speed from the sensors
-    measured_speed = hall_sensors.get_wheel_speed()
-
-    # Calculate the control signal using PID
-    #control_signal = pid.controller(measured_speed)
-    control_signal = 100
-    # Apply the control signal to the engine
-    engine.moveEngine(control_signal)
-
-    # Log the current state for debugging
-    print(f"Measured Speed: {measured_speed:.2f} km/h, Current Speed: {current_speed:.2f} km/h")
-    print(f"Control Signal: {control_signal}")
-
-    #current_speed = measured_speed
-    #current_speed += (measured_speed - current_speed) * 0.1
-
-    # Re-agendar a execução do controle após o tempo de amostragem
-    if running:
-        timer = threading.Timer(sampling_period, routine)
-        timer.start() """
-
-""" def calibrate_system():
+def calibrate_system(vehicle_control, hall_sensors):
     print("Iniciando calibração do sistema...")
     success = True
 
     try:
         # Teste do motor
         print("Testando Sensor_hall_motor...")
-        engine.moveEngine(50)
+        vehicle_control.set_engine_mode(GPIO.HIGH, GPIO.LOW, 50, 0)  # Motor a 50% para frente
         time.sleep(2)
         motor_rpm = hall_sensors.get_engine_rpm()
-        engine.moveEngine(0)
+        vehicle_control.set_engine_mode(GPIO.LOW, GPIO.LOW, 0, 0)  # Desliga o motor
+
         if motor_rpm > 0:
             print(f"Sensor_hall_motor funcionando: RPM = {motor_rpm:.2f}")
         else:
@@ -121,10 +88,11 @@ def routine():
 
         # Teste das rodas
         print("Testando sensores de roda...")
-        engine.moveEngine(30)
+        vehicle_control.set_engine_mode(GPIO.HIGH, GPIO.LOW, 30, 0)  # Motor a 30% para frente
         time.sleep(3)
-        wheel_speed = hall_sensors.compute_velocity()
-        engine.moveEngine(0)
+        wheel_speed = hall_sensors.compute_wheel_velocity()
+        vehicle_control.set_engine_mode(GPIO.LOW, GPIO.LOW, 0, 0)  # Desliga o motor
+
         if wheel_speed != 0:
             print(f"Sensores de roda funcionando: Velocidade = {wheel_speed:.2f} km/h")
         else:
@@ -133,7 +101,10 @@ def routine():
 
         # Verificação dos pedais
         print("Testando pedais...")
-        if not pedals.check_pedals():
+        pedal_ac = vehicle_control.read_accelerator_pedal()
+        pedal_fr = vehicle_control.read_brake_pedal()
+
+        if pedal_ac == 0 and pedal_fr == 0:
             print("Erro: Nenhum sinal dos pedais!")
             success = False
         else:
@@ -144,7 +115,7 @@ def routine():
         success = False
 
     print("Calibração concluída.")
-    return success """
+    return success
 
 
 def close():
@@ -155,26 +126,10 @@ def close():
         routine_thread.join()
 
     # Desligar componentes
-    #engine.moveEngine(0)
     hall_sensors.stop()
-    #pedals.stop()
     vehicle_control.cleanup()
     time.sleep(1)
     print('Sistema encerrado')
-
-""" def close_com_timer():
-    global running, timer
-    running = False
-
-    if timer:
-        timer.cancel()
-
-    # Desligar componentes
-    engine.moveEngine(0)
-    hall_sensors.stop()
-    pedals.stop()
-    time.sleep(1)
-    print('Sistema encerrado') """
 
 if __name__ == '__main__':
     main()
